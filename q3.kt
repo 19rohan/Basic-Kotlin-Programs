a518f9bfd59ee85a87704691b55380434161e943
Rohan-Mohanta <mohantarohan84@gmail.com>
2026-05-28 15:30:18 +0530

    function to sum all the numbers from 1 to N

--- q3.kt
+++ q3.kt
@@ -1,11 +1,11 @@
-fun sumnum(): Int{
+fun sum(n: Int): Int{
     var sum = 0
-    for (i in 1..10){
+    for (i in 1..n){
         sum+=i
     }
     return sum
 }
 fun main(){
-    var result:Int = sumnum()
-    println(result)
+    var result = sum(5)
+    println("sum from 1 to 5: $result")
 }
